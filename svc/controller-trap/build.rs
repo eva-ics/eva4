ef57151e4b4992204558fe6f36da881ab29a268e
Sergei S <div@altertech.com>
2022-10-09 19:00:00 +0200

    deps for native builds

--- svc/controller-trap/build.rs
+++ svc/controller-trap/build.rs
@@ -8,7 +8,10 @@ fn main() {
     let arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
     let target = env::var("TARGET").unwrap();
     if target == "x86_64-unknown-linux-gnu" {
+        println!("cargo:rustc-link-search=/opt/libnetsnmp/{}-{}", os, arch);
         println!("cargo:rustc-link-lib=netsnmp");
+        println!("cargo:rustc-link-arg=-lc");
+        println!("cargo:rustc-link-arg=-lbsd");
     } else {
         match os.as_str() {
             "linux" | "freebsd" => println!("cargo:rustc-link-lib=static=netsnmp"),
--- svc/controller-w1/build.rs
+++ svc/controller-w1/build.rs
@@ -6,18 +6,13 @@ use std::env;
 fn main() {
     let os = env::var("CARGO_CFG_TARGET_OS").unwrap();
     let arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
-    let target = env::var("TARGET").unwrap();
-    if target == "x86_64-unknown-linux-gnu" {
-        println!("cargo:rustc-link-lib=owcapi");
-        println!("cargo:rustc-link-lib=ow");
-    } else {
-        match os.as_str() {
-            "linux" | "freebsd" => {
-                println!("cargo:rustc-link-lib=static=owcapi");
-                println!("cargo:rustc-link-lib=static=ow");
-            }
-            _ => unimplemented!(),
-        };
-        println!("cargo:rustc-link-search=/opt/libow/{}-{}", os, arch);
-    }
+    //let target = env::var("TARGET").unwrap();
+    match os.as_str() {
+        "linux" | "freebsd" => {
+            println!("cargo:rustc-link-lib=static=owcapi");
+            println!("cargo:rustc-link-lib=static=ow");
+        }
+        _ => unimplemented!(),
+    };
+    println!("cargo:rustc-link-search=/opt/libow/{}-{}", os, arch);
 }
