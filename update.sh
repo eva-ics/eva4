322ab229d80267cd52169c4853be6e9271b7153e
Serhij S <div@altertech.com>
2023-04-12 00:01:00 +0200

    fmt

--- update.sh
+++ update.sh
@@ -108,7 +108,7 @@ if [ ! -f /.eva_container ]; then
     echo "---------------------------------------------"
     echo "Update completed. Starting everything back"
     # update eva4 config for systemd
-    if command -v systemctl; then
+    if command -v systemctl > /dev/null 2>&1; then
       if systemctl -a |grep " eva4.service " > /dev/null 2>&1; then
         if [ -f ./etc/eva_config ]; then
           if ! grep "^SYSTEMD_EVA4_SERVICE=" ./etc/eva_config > /dev/null 2>&1; then
