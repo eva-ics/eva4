574ae9b7e9ec00796502a1e3a21cfc9cf627d814
Serhij S <div@altertech.com>
2023-06-01 18:30:46 +0200

    metadata check for arbitrary_precision

---
+++ svc/hmi/build.rs
@@ -0,0 +1,30 @@
+use cargo_metadata::{CargoOpt, MetadataCommand};
+use std::path::Path;
+
+fn main() {
+    let mut manifest = Path::new(&std::env::var("CARGO_MANIFEST_DIR").unwrap()).to_owned();
+    manifest.push("Cargo.toml");
+    let metadata = MetadataCommand::new()
+        .manifest_path(manifest)
+        .features(CargoOpt::AllFeatures)
+        .exec()
+        .unwrap();
+    if let Some(resolve) = metadata.resolve {
+        for node in &resolve.nodes {
+            if node.id.repr.starts_with("serde_json ") {
+                if node.features.iter().any(|v| v == "arbitrary_precision") {
+                    let deps = resolve
+                        .nodes
+                        .iter()
+                        .filter(|d| d.dependencies.contains(&node.id))
+                        .map(|d| d.id.repr.as_str())
+                        .collect::<Vec<&str>>();
+                    panic!(
+                        "serde_json arbitrary_precision MUST be off. Dependents: \n\n{}\n\n",
+                        deps.join("\n")
+                    );
+                }
+            }
+        }
+    }
+}
