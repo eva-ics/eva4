caae15571fcde631607e207736f106e40d41b8a7
Serhij S <div@altertech.com>
2023-09-13 19:58:48 +0200

    docker apps (via compose)

---
+++ svc/dapp/src/dc.rs
@@ -0,0 +1,152 @@
+use eva_common::{EResult, Error};
+use serde::{Deserialize, Serialize};
+use std::process::Stdio;
+use std::time::Duration;
+use tokio::io::{AsyncBufReadExt, BufReader};
+use tokio::task::JoinHandle;
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Default, Deserialize, Serialize)]
+pub enum Version {
+    V1,
+    #[default]
+    V2,
+}
+
+impl Version {
+    pub fn cmd_up(self) -> &'static str {
+        match self {
+            Version::V1 => "docker-compose up",
+            Version::V2 => "docker compose up --remove-orphans --quiet-pull",
+        }
+    }
+    pub fn cmd_down(self) -> &'static str {
+        match self {
+            Version::V1 => "docker-compose down",
+            Version::V2 => "docker compose down",
+        }
+    }
+}
+
+impl TryFrom<u16> for Version {
+    type Error = Error;
+    fn try_from(v: u16) -> EResult<Self> {
+        match v {
+            1 => Ok(Version::V1),
+            2 => Ok(Version::V2),
+            _ => Err(Error::unsupported("unsupported docker compose version")),
+        }
+    }
+}
+
+pub type IoReceiver = async_channel::Receiver<String>;
+
+pub struct App {
+    path: String,
+    version: Version,
+    timeout: Duration,
+    pid: Option<u32>,
+    fut_stdout: Option<JoinHandle<()>>,
+    fut_stderr: Option<JoinHandle<()>>,
+}
+
+impl App {
+    pub fn new(path: &str, version: Version, timeout: Duration) -> Self {
+        Self {
+            path: path.to_owned(),
+            version,
+            timeout,
+            pid: None,
+            fut_stdout: None,
+            fut_stderr: None,
+        }
+    }
+    pub async fn start(&mut self) -> EResult<(IoReceiver, IoReceiver)> {
+        if self.pid.is_some() {
+            return Err(Error::busy("app is already started"));
+        }
+        let cmd = format!("chdir \"{}\" && {}", self.path, self.version.cmd_up());
+        let mut child = tokio::process::Command::new("sh")
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .kill_on_drop(false)
+            .args(["-c", &cmd])
+            .spawn()?;
+        let Some(stdout) = child.stdout.take() else {
+            return Err(Error::io("Unable to create stdout reader"))
+        };
+        let mut stdout_reader = BufReader::new(stdout).lines();
+        let Some(stderr) = child.stderr.take() else {
+            return Err(Error::io("Unable to create stderr reader"))
+        };
+        let Some(ppid) = child.id() else {
+            return Err(Error::io("Unable to take process id"))
+        };
+        let mut stderr_reader = BufReader::new(stderr).lines();
+        let (tx_out, rx_out) = async_channel::bounded(1024);
+        let (tx_err, rx_err) = async_channel::bounded(1024);
+        let fut_stdout = tokio::spawn(async move {
+            while let Some(line) = match stdout_reader.next_line().await {
+                Ok(v) => v,
+                Err(e) => {
+                    let _r = tx_out.send(e.to_string()).await;
+                    return;
+                }
+            } {
+                let _r = tx_out.send(line).await;
+            }
+        });
+        let fut_stderr = tokio::spawn(async move {
+            while let Some(line) = match stderr_reader.next_line().await {
+                Ok(v) => v,
+                Err(e) => {
+                    let _r = tx_err.send(e.to_string()).await;
+                    return;
+                }
+            } {
+                let _r = tx_err.send(line).await;
+            }
+        });
+        self.fut_stdout.replace(fut_stdout);
+        self.fut_stderr.replace(fut_stderr);
+        self.pid.replace(ppid);
+        Ok((rx_out, rx_err))
+    }
+    pub async fn stop(&mut self) -> EResult<Option<bmart::process::CommandResult>> {
+        if let Some(pid) = self.pid.take() {
+            bmart::process::kill_pstree(pid, Some(self.timeout), true).await;
+            let cmd = format!("chdir \"{}\" && {}", self.path, self.version.cmd_down());
+            let result = bmart::process::command(
+                "sh",
+                ["-c", &cmd],
+                self.timeout,
+                bmart::process::Options::default(),
+            )
+            .await?;
+            self.stop_futs();
+            Ok(Some(result))
+        } else {
+            self.stop_futs();
+            Ok(None)
+        }
+    }
+    fn stop_futs(&mut self) {
+        macro_rules! stop {
+            ($f: expr) => {
+                if let Some(fut) = $f.take() {
+                    fut.abort();
+                }
+            };
+        }
+        stop!(self.fut_stdout);
+        stop!(self.fut_stderr);
+    }
+}
+
+impl Drop for App {
+    fn drop(&mut self) {
+        if let Some(pid) = self.pid.take() {
+            bmart::process::kill_pstree_sync(pid, true);
+        }
+        self.stop_futs();
+    }
+}
---
+++ svc/dapp/src/main.rs
@@ -0,0 +1,95 @@
+use eva_common::prelude::*;
+use eva_sdk::prelude::*;
+use serde::Deserialize;
+use std::path::{Path, PathBuf};
+
+err_logger!();
+
+const AUTHOR: &str = "Bohemia Automation";
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+const DESCRIPTION: &str = "Docker App Runner";
+
+mod dc;
+
+#[cfg(not(feature = "std-alloc"))]
+#[global_allocator]
+static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
+
+#[inline]
+fn default_compose_version() -> u16 {
+    2
+}
+
+#[derive(Deserialize)]
+#[serde(deny_unknown_fields)]
+struct Config {
+    path: String,
+    #[serde(default = "default_compose_version")]
+    compose_version: u16,
+}
+
+struct Handlers {
+    info: ServiceInfo,
+}
+
+#[async_trait::async_trait]
+impl RpcHandlers for Handlers {
+    async fn handle_call(&self, event: RpcEvent) -> RpcResult {
+        let method = event.parse_method()?;
+        //let payload = event.payload();
+        #[allow(clippy::match_single_binding)]
+        match method {
+            _ => svc_handle_default_rpc(method, &self.info),
+        }
+    }
+}
+
+#[allow(clippy::too_many_lines)]
+#[svc_main]
+async fn main(mut initial: Initial) -> EResult<()> {
+    let config: Config = Config::deserialize(
+        initial
+            .take_config()
+            .ok_or_else(|| Error::invalid_data("config not specified"))?,
+    )?;
+    let compose_version = config.compose_version.try_into()?;
+    let path = if config.path.starts_with('/') {
+        config.path
+    } else {
+        let eva_dir = Path::new(initial.eva_dir());
+        let mut d_app_dir = PathBuf::from(&eva_dir);
+        d_app_dir.push("runtime");
+        d_app_dir.push("dapp");
+        d_app_dir.push(config.path);
+        d_app_dir.to_string_lossy().to_string()
+    };
+    let info = ServiceInfo::new(AUTHOR, VERSION, DESCRIPTION);
+    let rpc = initial.init_rpc(Handlers { info }).await?;
+    let client = rpc.client().clone();
+    svc_init_logs(&initial, client.clone())?;
+    info!("starting Docker compose with app in {}", path);
+    svc_start_signal_handlers();
+    let mut app = dc::App::new(&path, compose_version, initial.timeout());
+    let (tx_out, tx_err) = app.start().await?;
+    let client_c = client.clone();
+    svc_mark_ready(&client).await?;
+    tokio::spawn(async move {
+        while let Ok(s) = tx_out.recv().await {
+            info!("APP: {}", s);
+        }
+        if !svc_is_terminating() {
+            error!("app terminated");
+            let _ = svc_mark_terminating(&client_c).await;
+        }
+    });
+    tokio::spawn(async move {
+        while let Ok(s) = tx_err.recv().await {
+            info!("APP: {}", s);
+        }
+    });
+    info!("{} started ({})", DESCRIPTION, initial.id());
+    svc_block(&rpc).await;
+    svc_mark_terminating(&client).await?;
+    app.stop().await?;
+    Ok(())
+}
