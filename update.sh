dd71d308c7abf406ab2066901cbe35aa455ee83a
Serhij S <div@altertech.com>
2023-10-16 20:50:24 +0200

    remove vendored apps on update

--- update.sh
+++ update.sh
@@ -94,6 +94,7 @@ if [ ! -f /.eva_container ]; then
   rm -f "_update/eva-${VERSION}/ui/favicon.ico"
   rm -f "_update/eva-${VERSION}/update.sh"
   rm -f ./cli/eva-cloud-manager
+  rm -rf vendored-apps
   cp -rf "_update/eva-${VERSION}/"* . || exit 1
 fi
 
