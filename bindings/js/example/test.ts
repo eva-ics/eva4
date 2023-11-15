0a7e4bcaeccd49aa340a7b9df3e157cae2f01957
Serhij S <div@altertech.com>
2023-11-15 01:48:42 +0100

    test service example

--- bindings/js/example/test.ts
+++ bindings/js/example/test.ts
@@ -4,6 +4,7 @@ import {
   Service,
   pack,
   unpack,
+  EventKind,
   Action,
   XCall,
   XCallData,
@@ -11,6 +12,7 @@ import {
   RpcEvent
 } from "@eva-ics/sdk";
 
+/** Bus frame handler */
 const onFrame = async (e: RpcEvent): Promise<void> => {
   console.log(e);
 };
@@ -22,11 +24,13 @@ const main = async () => {
     const method = e.method?.toString();
     const payload = unpack(e.getPayload());
     switch (method) {
+      // process HTTP x-calls
       case "x":
         const xcall = new XCall(payload as XCallData);
         xcall.acl.requireAdmin();
         console.log(xcall);
         return pack({ ok: true });
+      // process lmacro executions
       case "run":
         const lAction = new Action(payload);
         console.log(lAction);
@@ -38,14 +42,17 @@ const main = async () => {
           -15
         );
         return;
+      // process unit actions
       case "action":
         const uAction = new Action(payload);
         console.log(uAction);
         service.controller.eventRunning(uAction);
         service.controller.eventCompleted(uAction, "all fine");
         return;
+      // example RPC method which deals with the params payload
       case "hello":
         return payload?.name ? pack(`hello ${payload.name}`) : pack("hi there");
+      // get service initial payload
       case "config":
         return pack(service.initial);
       default:
@@ -56,13 +63,14 @@ const main = async () => {
     info: new ServiceInfo({
       author: "Bohemia Automation",
       description: "Test JS service",
-      version: "0.1"
-    }).addMethod(new ServiceMethod("hello").optional("name")),
+      version: "0.0.1"
+    })
+      .addMethod(new ServiceMethod("hello").optional("name"))
+      .addMethod(new ServiceMethod("config")),
     onFrame: onFrame,
     onRpcCall: onRpcCall
   });
-  //await service.subscribeOIDs(["#"], EventKind.Any);
-  await service.waitCore(false, 2);
+  await service.subscribeOIDs(["#"], EventKind.Any);
   await service.block();
   console.log("exiting");
   process.exit(0);
