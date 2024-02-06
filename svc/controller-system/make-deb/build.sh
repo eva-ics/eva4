01a77c001aca8af34da9a7e97b9af16f040ee84d
Serhij S <div@altertech.com>
2024-02-07 00:55:10 +0100

    release files and deb build

---
+++ svc/controller-system/make-deb/build.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -xe
+
+AGENT=..
+
+VERSION=$(grep ^version ${AGENT}/Cargo.toml|awk '{ print $3 }'|tr -d '"')
+BUILD=$(cat ${AGENT}/build.number)
+DESCRIPTION=$(grep "^const DESCRIPTION" ${AGENT}/src/agent/linux.rs | awk -F\" '{ print $2 }')
+
+[ -z "$VERSION" ] && exit 1
+[ -z "$BUILD" ] && exit 1
+[ -z "$DESCRIPTION" ] && exit 1
+
+TARGET="${PACKAGE}-${VERSION}-${BUILD}-amd64${PACKAGE_SUFFIX}"
+[ -z "${RUST_TARGET}" ] && RUST_TARGET=x86_64-unknown-linux-musl
+[ -z "${TARGET_DIR}" ] && TARGET_DIR=target
+
+rm -rf "./${TARGET}"
+mkdir -p "./${TARGET}/usr/sbin"
+mkdir -p "./${TARGET}/lib/systemd/system"
+mkdir -p "./${TARGET}/DEBIAN"
+cp -vf "${AGENT}/${TARGET_DIR}/${RUST_TARGET}/release/eva-cs-agent-linux" "./${TARGET}/usr/sbin/eva-cs-agent"
+cp -vf "${AGENT}/eva-cs-agent.service" "./${TARGET}/lib/systemd/system/"
+mkdir -p "./${TARGET}/etc/eva-cs-agent"
+cp "${AGENT}/agent-config-example.yml" "./${TARGET}/etc/eva-cs-agent/config.yml-dist"
+(
+cat << EOF
+Package: ${PACKAGE}
+Version: ${VERSION}-${BUILD}
+Section: base
+Priority: optional
+Architecture: amd64
+Maintainer: Serhij S. <div@altertech.com>
+Description: ${DESCRIPTION}
+EOF
+) > "./${TARGET}/DEBIAN/control"
+cp -vf ./debian/* "./${TARGET}/DEBIAN/"
+dpkg-deb --build "./${TARGET}"
---
+++ svc/controller-system/release.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+VERSION=$(grep ^version ./Cargo.toml|awk '{ print $3 }'|tr -d '"')
+BUILD=$(cat ./build.number)
+
+URI=pub.bma.ai/eva-cs-agent
+
+
+[ -z "$VERSION" ] && exit 1
+[ -z "$BUILD" ] && exit 1
+
+LINUX_BINARY_X86_64=eva-cs-agent-linux-${VERSION}-${BUILD}-x86_64
+LINUX_BINARY_AARCH64=eva-cs-agent-linux-${VERSION}-${BUILD}-aarch64
+DEBIAN_PACKAGE_X86_64=eva-cs-agent-4.0.2-1-amd64.deb
+
+( curl -sI "https://${URI}/${LINUX_BINARY_X86_64}" 2>&1|head -1|grep " 404 " ) > /dev/null 2>&1
+
+if [ $? -ne 0 ]; then
+  echo "Build ${BUILD} already released"
+  exit 2
+fi
+
+make compile-musl-x86_64 || exit 3
+make compile-musl-aarch64 || exit 3
+make debian-pkg || exit 3
+
+gsutil cp -a public-read \
+  ./target-x86_64-musl/x86_64-unknown-linux-musl/release/eva-cs-agent-linux \
+  "gs://${URI}/${LINUX_BINARY_X86_64}" || exit 4
+
+gsutil cp -a public-read \
+  ./target-aarch_64-musl/aarch64-unknown-linux-musl/release/eva-cs-agent-linux \
+  "gs://${URI}/${LINUX_BINARY_AARCH64}" || exit 4
+
+gsutil cp -a public-read \
+  "./_build/${DEBIAN_PACKAGE_X86_64}" \
+  "gs://${URI}/${DEBIAN_PACKAGE_X86_64}" || exit 4
+
+rci job run pub.bma.ai || exit 5
+
+echo ""
+echo "Release successful"
