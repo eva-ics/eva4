47b8308805d7073ed72a5452ed3a7b9e6ec49185
Serhij S <div@altertech.com>
2024-04-27 03:17:40 +0200

    stable build

--- eva/src/lib.rs
+++ eva/src/lib.rs
@@ -23,7 +23,7 @@ pub const PRODUCT_NAME: &str = "EVA ICS node server";
 pub const PRODUCT_CODE: &str = "eva4node";
 pub const VERSION: &str = env!("CARGO_PKG_VERSION");
 #[allow(clippy::unreadable_literal)]
-pub const BUILD: u64 = 2024042702;
+pub const BUILD: u64 = 2024042703;
 pub const AUTHOR: &str = "(c) 2022 Bohemia Automation / Altertech";
 
 pub const SYSINFO_CHECK_INTERVAL: Duration = Duration::from_secs(5);
