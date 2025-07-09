52b758accc79676b5f3b85afd976d843dbb75ede
Serhij S <s@g5.is>
2025-07-09 22:53:16 +0200

    eva-shell ver up

--- eva-shell/eva4_shell/__init__.py
+++ eva-shell/eva4_shell/__init__.py
@@ -1,3 +1,3 @@
-__version__ = '0.2.35'
+__version__ = '0.2.36'
 
 DEFAULT_REPOSITORY_URL = 'https://pub.bma.ai/eva4'
--- eva-shell/setup.py
+++ eva-shell/setup.py
@@ -1,4 +1,4 @@
-__version__ = '0.2.35'
+__version__ = '0.2.36'
 
 import setuptools
 
