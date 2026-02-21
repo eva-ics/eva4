c258f9a4f8d4ca8d4c0910b0265f46de65cd8fb3
Serhij S <s@g5.is>
2026-02-21 03:53:28 +0100

    allow svc methods to be string for wildcard

--- svc/eva-mcp/src/allow.rs
+++ svc/eva-mcp/src/allow.rs
@@ -9,17 +9,57 @@ pub enum Allow {
 }
 
 #[derive(Clone, Debug)]
-pub(crate) struct AllowRules(HashMap<String, Vec<String>>);
+pub(crate) struct AllowRules(HashMap<String, MethodsList>);
+
+#[derive(Clone, Debug)]
+pub(crate) struct MethodsList(Vec<String>);
 
 impl Allow {
     pub fn allows(&self, target: &str, method: &str) -> bool {
         match self {
             Allow::All => true,
-            Allow::Rules(r) => r
-                .0
-                .get(target)
-                .is_some_and(|methods| methods.iter().any(|m| m == "*" || m == "#" || m == method)),
+            Allow::Rules(r) => r.0.get(target).is_some_and(|methods| {
+                methods.0.iter().any(|m| m == "*" || m == "#" || m == method)
+            }),
         }
+    }
+}
+
+impl<'de> Deserialize<'de> for MethodsList {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct MethodsVisitor;
+        impl<'de> Visitor<'de> for MethodsVisitor {
+            type Value = MethodsList;
+
+            fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+                formatter.write_str(r##""*" or "#" or ["method", ...]"##)
+            }
+
+            fn visit_str<E>(self, v: &str) -> Result<MethodsList, E>
+            where
+                E: de::Error,
+            {
+                if v == "*" || v == "#" {
+                    Ok(MethodsList(vec![v.to_string()]))
+                } else {
+                    Err(de::Error::custom(
+                        "allow method value must be \"*\" or \"#\" (all methods) or a list",
+                    ))
+                }
+            }
+
+            fn visit_seq<A>(self, seq: A) -> Result<MethodsList, A::Error>
+            where
+                A: de::SeqAccess<'de>,
+            {
+                let v = Vec::deserialize(de::value::SeqAccessDeserializer::new(seq))?;
+                Ok(MethodsList(v))
+            }
+        }
+        deserializer.deserialize_any(MethodsVisitor)
     }
 }
 
@@ -33,7 +73,7 @@ impl<'de> Deserialize<'de> for Allow {
             type Value = Allow;
 
             fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-                formatter.write_str(r##"allow: "*" or "#" or { "target": ["method", ...], ... }"##)
+                formatter.write_str(r##"allow: "*" or "#" or { "target": "*"|"#"|["method", ...], ... }"##)
             }
 
             fn visit_str<E>(self, v: &str) -> Result<Allow, E>
